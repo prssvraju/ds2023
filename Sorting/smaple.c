2c329b7ddc1506e43f92212701ffdc9caadd3c7a
prssvraju <prssvraju2@gmail.com>
2023-08-17 09:44:01 +0530

    update matrix passing

--- Sorting/smaple.c
+++ Sorting/smaple.c
@@ -1,8 +1,33 @@
 #include<stdio.h>
+void create_OneDArr(int[],int);
+void print_OneDArr(int[],int);
+
 main()
 {
-    printf("Enter name");
-    printf("Enter nameSss");
-        printf("Enter from mac ari M1");
+    int n,i;
+    int a[10];
+    printf("Enter the size of array");
+    scanf("%d",&n);
+    create_OneDArr(a,n);
+    print_OneDArr(a,n);
+   
+
+}
+
+void create_OneDArr(int a[10],int n)
+{
+    int i;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+}
+void print_OneDArr(int a[10],int n)
+{
+    int i;
+    for(i=0;i<n;i++)
+    {
+        printf("%d",a[i]);
+    }
 
 }
