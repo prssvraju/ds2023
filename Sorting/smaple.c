299568eb9321f8c6d2eec758f1261a6d72917bfc
PRSSVenkatapahiRaju <prssvraju2@gmail.com>
2023-08-18 14:02:13 +0530

    factupdate

--- Sorting/smaple.c
+++ Sorting/smaple.c
@@ -1,6 +1,33 @@
 #include<stdio.h>
-main()
+int* readArr(int);
+void printArr(int[],int);
+int main()
 {
-    printf("Enter name");
-    printf("Enter nameSss");
+  int n,i;
+  int *a;
+  printf("Enter n value ");
+  scanf("%d",&n);
+  a=readArr(n);
+  printArr(a,n);
+  return 0;
 }
+void printArr(int a[10],int n)
+{
+    int i;
+    for(i=0;i<n;i++)
+    {
+        printf("%d\t",*(a+i));
+    }
+}
+
+int* readArr(int n)
+{
+    int i;
+    int al[10];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",al[i]);
+    }
+    return al;
+}
+
