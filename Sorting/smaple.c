73db87419733fd0f9146733f57722711403d8515
prssvraju <prssvraju2@gmail.com>
2023-08-20 16:57:28 +0530

    update air

--- Sorting/smaple.c
+++ Sorting/smaple.c
@@ -1,8 +1,26 @@
 #include<stdio.h>
-main()
+int* readArr(int);
+int main()
 {
-    printf("Enter name");
-    printf("Enter nameSss");
-        printf("Enter from mac ari M1");
+  int *a, n,i;
+  printf("Enter the size of arrray");
+  scanf("%d",&n);
+  a=readArr(n);
+  for(i=0;i<n;i++)
+  {
+    printf("%d",a[i]);
+  }
+  return 0;
+}
+int* readArr(int n)
+{
+    int a[10];
+    int *res =a;
+    int i;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
 
+    return res;
 }
