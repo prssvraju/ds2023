b3265fa2802a466c07af023831ae6ae1e1bbdd8c
PRSSVenkatapahiRaju <prssvraju2@gmail.com>
2023-08-25 15:57:32 +0530

    update

--- linearitc.c
+++ linearitc.c
@@ -3,6 +3,8 @@ int l_search(int[],int,int);
 int lr_search(int[],int,int);
 int b_search(int[],int,int);
 int br_search(int[],int,int,int);
+int f_search(int[],int,int);
+int getFib(int);
 int main()
 {
     int n,a[10],k,i,index;
@@ -15,7 +17,7 @@ int main()
     }
     printf("Enter key to search");
     scanf("%d",&k);
-    index=br_search(a,0,n-1,k);
+    index=f_search(a,n,k);
     //res=linearsearch(a,n,k);
     if(index == -1)
     {
@@ -105,3 +107,58 @@ int br_search(int a[10],int low,int high,int k)
     }
 
 }
+
+int getFib(int n)
+{
+    int fb1,fb2,fbn,i;
+    fb1=0;
+    fb2=1;
+    if(n==0||n==1)
+    {
+        return 0;
+    }
+    for(i=0;i<n;i++)
+    {
+        fbn=fb1+fb2;
+        fb1=fb2;
+        fb2=fbn;
+    }
+    return fb1;
+}
+
+int f_search(int a[10],int n,int k)
+{
+    int low,high,loc,flag,index;
+    low=0;
+    high=n-1;
+    loc=-1;
+    flag=0;
+    while(low<=high && flag!=1)
+    {
+        index=getFib(n);
+        if(a[index+low]==k)
+        {
+            flag=1;
+            loc=index+low;
+        }
+        else if(a[index+low]>k)
+        {
+            high=index+low-1;
+        }
+        else
+        {
+            low=index+low+1;
+        }
+        n=high-low+1;
+    }
+    if(flag == 1)
+    {
+        return loc;
+    }
+    else
+    {
+        return -1;
+    }
+
+}
+
